ed95dd0597d2313d42c0f28ee8f8da7396f27b6f
lheidgal <lheidgal@outlook.com>
2024-06-08 13:12:08 +0800

    eda

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# heidga
+# Mindustry-mod
+-    Field-Expansion
+-    This module is compatible with most versions
+-    Greed expands your visible area in the game
+-     This module is compatible with multiplayer gaming modes
---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+//Le Zooom
+
+const defaultMinZoomLim = Vars.renderer.minZoom;
+const defaultMaxZoomLim = Vars.renderer.maxZoom;
+print("default min zoom: "+defaultMinZoomLim);
+print("defaultn max zoom: "+defaultMaxZoomLim);
+
+const minZoomLim = 0.5;
+const maxZoomLim = 25;
+
+// default extended zoom limits
+const minZoom = 0.75;
+const maxZoom = 20;
+
+function resetZoomLim(toOriginal){
+	if(toOriginal){
+		Vars.renderer.minZoom = defaultMinZoomLim;
+		Vars.renderer.maxZoom = defaultMaxZoomLim;
+	} else {
+		Vars.renderer.minZoom = minZoomLim;
+		Vars.renderer.maxZoom = maxZoomLim;
+	}
+}
+
+
+function updateZoom(min, max){
+	Vars.renderer.minZoom = min;
+	Vars.renderer.maxZoom = max;
+}
+
+if(!Vars.headless){
+	updateZoom(minZoomLim,maxZoomLim);
+}
